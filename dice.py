160be4d218d78605a103793ea1b92a84a4ba4b27
Haley Marquez <89376070+Marquez-Haley@users.noreply.github.com>
2025-04-30 11:53:43 -0700

    created main program

---
+++ dice.py
@@ -0,0 +1,24 @@
+import random
+
+class Dice:
+    def __init__(self, sides):
+        if sides % 2 == 0:
+            self.sides = sides
+        else:
+            self.sides = None
+
+        self.total = 0
+
+    def roll(self):
+        if self.sides == None:
+            print("Dice is not an even number!")
+        else:
+            number = random.randint(1, self.sides)
+            self.total += number
+        return number
+    
+    def get_total(self):
+        return self.total
+    
+    def clear(self):
+        self.total = 0
---
+++ main.py
@@ -0,0 +1,38 @@
+from dice import Dice
+
+def main():
+    print("Welcome to Dice Roller!")
+    print("************************")
+    print("1. 6 Sided Dice")
+    print("2. 20 Sided Dice")
+    print("3. Custom dice")
+    userChoice = input("Select an option to start: ")
+    # if userChoice == 3:
+    #     customSides = input("Please input an even number of sides")
+
+    if userChoice == '1':
+        new_dice = Dice(6)
+    elif userChoice == '2':
+        new_dice = Dice(20)
+    else:
+        new_dice = Dice(customSides)
+
+
+    while(True):
+        print("1. Roll")
+        print("2. Clear")
+        print("3. Exit")
+        diceChoice = input()
+
+        if diceChoice == '1':
+            rolledNum = new_dice.roll()
+            print("You rolled: ", rolledNum)
+            print("Total: ", new_dice.total)
+        elif diceChoice == '2':
+            new_dice.clear()
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
