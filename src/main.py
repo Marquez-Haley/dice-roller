bd174730a7ea39a6acd7c8d578fc6b6535c8bb8c
Haley Marquez <hjm.marquez@gmail.com>
2025-05-06 18:42:57 -0700

    small fixes

--- src/main.py
+++ src/main.py
@@ -6,22 +6,26 @@ def main():
     print("1. 6 Sided Dice")
     print("2. 20 Sided Dice")
     print("3. Custom dice")
+    print("Press any other key to exit.")
     userChoice = input("Select an option to start: ")
    
     if userChoice == '1':
         new_dice = Dice(6)
     elif userChoice == '2':
         new_dice = Dice(20)
-    else:
+    elif userChoice == '3':
        customSides = input("Please input an even number of sides")
        new_dice = Dice(customSides)
+    else:
+        return()
+
 
 
     while(True):
         print("1. Roll")
         print("2. Clear")
         print("3. Undo Last Roll")
-        print("3. Exit")
+        print("4. Exit")
         diceChoice = input()
 
         if diceChoice == '1':
@@ -33,13 +37,16 @@ def main():
                 print("Total: ", new_dice.get_total())
         elif diceChoice == '2':
             new_dice.clear()
+            print("Total: ", new_dice.get_total())
         elif diceChoice == '3':
             if new_dice.undo_last_roll():
                 print("Total: ", new_dice.get_total())
             else:
                 print("Nothing to undo! Try rolling first!")
+        elif diceChoice == '4':
+            return()
         else:
-            break
+            print("Not a valid choice!")
 
 
 if __name__ == "__main__":
